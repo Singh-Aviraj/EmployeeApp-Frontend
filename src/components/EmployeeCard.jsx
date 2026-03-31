30e3b7c2578f2bee47aae04a76e4cda4874cdf20
Aviraj Singh <148427283+Singh-Aviraj@users.noreply.github.com>
2026-03-31 20:18:19 +0530

    Update EmployeeCard.jsx

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -8,22 +8,22 @@ function EmployeeCard({ employee, onDelete }) {
   return (
     <div className="bg-white rounded-2xl shadow-md p-5 w-full max-w-md mx-auto mb-6 hover:shadow-lg transition duration-300">
       <h2 className="text-xl font-semibold text-gray-800 mb-1">{employee.name}</h2>
-      <p className="text-gray-600 mb-1">👔 {employee.designation}</p>
-      <p className="text-gray-600 mb-1">📧 {employee.email}</p>
-      <p className="text-gray-600 mb-4">📞 {employee.phoneNumber}</p>
+      <p className="text-gray-600 mb-1"> {employee.designation}</p>
+      <p className="text-gray-600 mb-1"> {employee.email}</p>
+      <p className="text-gray-600 mb-4"> {employee.phoneNumber}</p>
 
       <div className="flex justify-end gap-3">
         <button
           onClick={() => navigate(`/update/${employee.id}`)}
-          className="bg-green-600 hover:bg-green-800 text-white px-4 py-1 rounded-xl text-sm"
+          className="bg-green-700 hover:bg-green-900 text-white px-4 py-1 rounded-xl text-sm"
         >
-          Update
+          UPDATE
         </button>
         <button
           onClick={() => onDelete(employee.id)}
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-1 rounded-xl text-sm"
+          className="bg-red-600 hover:bg-red-800 text-white px-4 py-1 rounded-xl text-sm"
         >
-          Delete
+          DELETE
         </button>
       </div>
     </div>
